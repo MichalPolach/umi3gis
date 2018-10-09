0fff60a46239d39159273b4a22710d3d15f21b90
MichalPolach <39467299+MichalPolach@users.noreply.github.com>
2018-10-09 12:54:59 +0200

    silly grammar error
    such me, much cringe

--- umi3gis.sh
+++ umi3gis.sh
@@ -6,7 +6,7 @@ echo "========== CHECKING UPDATES =========="
 sudo apt update
 
 echo
-echo "========== INSTALLING DEPENDENCYS & SOFTWARE =========="
+echo "========== INSTALLING DEPENDENCIES & SOFTWARE =========="
 sudo apt install -y vim libxcb1-dev libxcb-keysyms1-dev libpango1.0-dev libxcb-util0-dev \
 libxcb-icccm4-dev libyajl-dev libstartup-notification0-dev libxcb-randr0-dev libev-dev \
 libxcb-cursor-dev libxcb-xinerama0-dev libxcb-xkb-dev libxkbcommon-dev libxkbcommon-x11-dev \
